f43e3c13c222cccfeff6c77004431137cd5c0d81
andressamarques <andressa.alves.aa.457@gmail.com>
2016-07-29 07:36:39 -0300

    Números Primos
    trabalho Antoanne, num. primos C#

---
+++ Prime-numbers-master/Prime numbers/Prime numbers/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Primos p = new Primos();
+
+            Console.WriteLine("desejas tu até q numero?");
+            p.limit = int.Parse(Console.ReadLine());
+
+            p.initNumbers(p.limit);
+
+            p.markAndDelete();
+
+            p.numbersAsString();
+        }
+    }
+}
